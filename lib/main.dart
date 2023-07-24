58f66599ad85abc4c723d870191aa414623b53c2
kan15hka <kanishka2727@gmail.com>
2023-07-24 23:08:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
+
+import 'package:meals/screens/tabs.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  fontFamily: GoogleFonts.montserrat().fontFamily,
+  textTheme: const TextTheme(
+      headlineLarge: TextStyle(fontSize: 18.0),
+      titleLarge: TextStyle(fontSize: 20.0)),
+);
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(
+            const ProviderScope(
+              child: App(),
+            ),
+          ));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const TabsScreen(),
+    );
+  }
+}
